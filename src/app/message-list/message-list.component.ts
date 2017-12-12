c66567b541ec1fb00328d5a495aa005a46eb3444
Franck Libam <franck.libam@gmail.com>
2017-12-12 03:01:59 +0100

    ref: create message list component

---
+++ src/app/message-list/message-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mcb-message-list',
+  templateUrl: './message-list.component.html',
+  styles: []
+})
+export class MessageListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
